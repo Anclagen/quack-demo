99450319ad22014487d2abc0e988cf675de43bf2
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-09-13 02:39:17 +0200

    setup job page

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,5 +1,28 @@
+import Banner from "../components/Banner/Banner";
+import Section from "../components/Sections/Section";
 const Jobs = () => {
-  return <h1>Jobs</h1>;
+  return (
+    <>
+      <Banner title="Jobs" slogan="Discover Your Next Opportunity, Let's Shape Your Career Path" imgSrc="/src/assets/pexels-edmond-dantès-4344878.jpg" imgAlt="Descriptive Image Alt Text" />
+      <Section bgColor="black bg-opacity-80">
+        <h2>Our Listings</h2>
+        <div className="relative h-screen max-w-3xl mx-auto">
+          <iframe
+            src="https://www.cv-library.co.uk/cgi-bin/yourjobs.cgi?agency=309161"
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              height: "100%",
+              width: "100%",
+              border: "none",
+            }}
+            title="Full Page Iframe"
+          ></iframe>
+        </div>
+      </Section>
+    </>
+  );
 };
 
 export default Jobs;
