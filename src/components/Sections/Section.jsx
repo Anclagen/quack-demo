62ea1edcb43c0c64a6e2aab5de0785579a0e31c3
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-09-13 02:32:20 +0200

    Section component

---
+++ src/components/Sections/Section.jsx
@@ -0,0 +1,9 @@
+const Section = ({ bgColor, children }) => {
+  return (
+    <section className={`w-full overflow-hidden px-4 py-24 ${bgColor && `bg-${bgColor}`}`}>
+      <div className="max-w-screen-xl mx-auto">{children}</div>
+    </section>
+  );
+};
+
+export default Section;
