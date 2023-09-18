60bb36970a1ce05d04c796882c8c0a792c24489f
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-09-18 18:22:33 +0200

    updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,68 @@
 import Banner from "../components/Banner/Banner";
 import Section from "../components/Sections/Section";
 import bannerImage from "../assets/pexels-olga-lioncat-7245326.jpg";
+import TwoColSection from "../components/Sections/StandardSection/twoColSection";
+import fadeinImage from "../assets/pexels-yan-krukau-8867482.jpg";
+import uglyMugImage from "../assets/pexels-andrea-piacquadio-3778603.jpg";
+import CardSection from "../components/Sections/CardSection/CardSection";
+import { CardWithLargerImageTop } from "../components/Card/Card";
+import Head from "../components/Head";
 
 const About = () => {
   return (
     <>
+      <Head title="About" description="A Dedicated & Experienced Team Of Experts" />
       <Banner title="About" slogan="A Dedicated & Experienced Team Of Experts" imgSrc={bannerImage} imgAlt="Descriptive Image Alt Text" />
-      <Section bgColor="bg-black bg-opacity-80">
-        <h2 className="text-3xl text-center mb-10">Who Are We</h2>
+      <Section bgColor="bg-zinc-950 text-white">
+        <TwoColSection
+          title="Who We Are"
+          imageSrc={fadeinImage}
+          imageAlt="Description"
+          text={[
+            "Quack Specialists are a dedicated team of expert consultants with specialisms in your sector, as opposed to a typical generic high street generalist recruiter, jack of all trades (master of none!)",
+            "We focus on three very specific industries as this has become our niche, these industries are; Meat, Engineering & Construction and Warehouse & Logistics. Our team work collectively in making sure that every step of the process is seamless from on boarding to candidate attrition management.",
+            "We are a GLAA License holder and our insurance levels are setup to support our clients needs as follows:",
+            "Public Liability - £10m",
+            "Employers' Liability - £10m",
+            "Professional Indemnity - £10m",
+            "To find out if we can help, get in touch with us today.",
+          ]}
+          linkText="Get In Touch"
+          linkHref="/contact"
+        />
       </Section>
-      <Section bgColor="bg-gray bg-opacity-10">
-        <h2 className="text-3xl text-center mb-10">Meet The Team</h2>
+      <Section bgColor="bg-zinc-200">
+        <TwoColSection
+          title="Meet The Team"
+          imageSrc={fadeinImage}
+          imageAlt="Description"
+          text={[
+            "At Quack Specialists Ltd, the backbone of our operations rests with our Senior Team. These dedicated professionals are often the driving force, working diligently behind the scenes, ensuring the seamless functioning of our business. Their responsibilities don't just end at overseeing daily operations. They're also proactive in addressing any queries, concerns, or challenges that arise, guaranteeing that our partners, clients, and candidates receive top-notch service at all times. ",
+            "We believe in transparency and fostering trust, so we invite you to 'virtually meet' and get acquainted with some of the key members of our Senior Team below. Get to know the minds and faces that are committed to your success!",
+          ]}
+          reverse={true}
+        />
+        <h3 className="text-2xl font-semibold text-center">Our Senior Team</h3>
+        <CardSection>
+          <CardWithLargerImageTop
+            title="Jamie Croall, Chief Financial Officer"
+            imgSrc={uglyMugImage}
+            imgAlt="Image 1 Description"
+            text="Jamie plays a pivotal role in the meticulous planning and management of our financial affairs, encompassing everything from ledgers and forecasts to projections. He leads a dedicated team focused on the critical aspects of our financial operations. This includes ensuring timely payments from our customers, settling accounts with our suppliers punctually, and most importantly, guaranteeing that our workers receive accurate and timely compensation."
+          />
+          <CardWithLargerImageTop
+            title="Lee Inskip, Chief Operations Officer"
+            imgSrc={uglyMugImage}
+            imgAlt="Image 1 Description"
+            text="Lee, boasting a wealth of sector experience even before the recruitment industry gained its prominence, is deeply involved in the day-to-day operations of the business. With a background in operational roles at top global recruitment agencies, Lee effortlessly liaises with clients. His expertise ensures that our consultants are always aligned with client needs, facilitating successful placements."
+          />
+          <CardWithLargerImageTop
+            title="Dion Bishop, Chief Information Officer"
+            imgSrc={uglyMugImage}
+            imgAlt="Image 1 Description"
+            text="Dion ensures the seamless operation of our internal systems, providing the backbone that our Consultants, Account Managers, Support Staff, and Finance Team rely on. With an array of different software solutions at his fingertips, Dion's expertise guarantees each team member can execute their roles with maximum efficiency."
+          />
+        </CardSection>
       </Section>
     </>
   );
