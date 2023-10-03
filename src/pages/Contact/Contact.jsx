83fa2bf78cce2a5ef250a403a5a8457a9b9e1d34
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-10-03 19:29:48 +0200

    added general contact form to contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import Section from "../../components/Sections/Section";
 import bannerImage from "../../assets/pexels-yan-krukau-8867434.jpg";
 import constructionImage from "../../assets/undraw_under_construction_46pa.png";
 import ClientForm from "../../components/ContactForms/ClientForm/ClientForm";
+import GeneralContactForm from "../../components/ContactForms/GeneralContactForm/GeneralContactForm";
 
 const Contact = () => {
   return (
@@ -12,9 +13,14 @@ const Contact = () => {
       <BannerHome title="Contact Us" slogan="Employer Or Job Seeker Were Here To Help" imgSrc={bannerImage} imgAlt="Descriptive Image Alt Text" links={[]} />
       <Section bgColor="bg-zinc-950">
         <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0">
-          <div className="px-6 rounded-lg text-white flex-none md:w-1/3">
-            <h2 className="text-3xl font-semibold mb-6">Contact Details</h2>
-            <div className="mb-4">
+          <div className="px-6 rounded-lg text-white flex-none md:w-1/2">
+            <h2 className="text-2xl font-bold leading-8 sm:text-3xl sm:leading-9 mb-6">Contact Details</h2>
+            <p className="mb-5">
+              {
+                "Whether you have a simple question or need detailed assistance, we're here to help. Feel free to call, email, or simply fill out our contact query. We value your communication and aim to respond as quickly as possible during our office hours."
+              }
+            </p>
+            <div className="mb-5">
               <h3 className="text-2xl font-semibold">E-mail</h3>
               <p className="mt-2 text-lg">
                 <a href="mailto:sales@quackspecialists.co.uk" className="text-violet-400 hover:underline">
@@ -22,9 +28,13 @@ const Contact = () => {
                 </a>
               </p>
             </div>
-            <div className="mb-4">
+            <div className="mb-5">
               <h3 className="text-2xl font-semibold">Phone</h3>
               <p className="mt-2 text-lg">+44 333 577 0036</p>
+            </div>
+            <div className="mb-5">
+              <h3 className="text-2xl font-semibold">Office Hours</h3>
+              <p className="mt-2 text-lg">Monday to Friday: 8am - 5.30pm</p>
             </div>
             <div>
               <h3 className="text-2xl font-semibold">Legal / Head office</h3>
@@ -43,16 +53,33 @@ const Contact = () => {
               </address>
             </div>
           </div>
-
-          <div className="flex-1 md:w-2/3">
-            <h2 className="text-3xl text-white font-semibold mb-10 text-center">Serviced Locations & Offices</h2>
-            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1Jt4IhkKUr8cE6T5kf97Rs3f8TqFwoMA&ehbc=2E312F&noprof=1" className="w-full" height={"500px"}></iframe>
+          <div className="flex-1 md:w-1/2">
+            <h2 className="text-2xl font-bold leading-8 sm:text-3xl sm:leading-9 mb-6 text-white text-center">Get In Touch</h2>
+            <GeneralContactForm />
           </div>
         </div>
       </Section>
       <Section bgColor="bg-gray bg-opacity-10 text-center">
-        <h2 className="text-3xl mb-10">Got A Position To Fill</h2>
-        <ClientForm />
+        <h2 className="text-2xl font-bold leading-8 text-black sm:text-3xl sm:leading-9 text-center mb-10">Serviced Locations & Offices</h2>
+        <div
+          className="relative max-w-screen-lg mx-auto"
+          style={{
+            height: "70vh",
+          }}
+        >
+          <iframe
+            src="https://www.google.com/maps/d/u/0/embed?mid=1Jt4IhkKUr8cE6T5kf97Rs3f8TqFwoMA&ehbc=2E312F&noprof=1"
+            title="Offices & Serviced Locations"
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              height: "100%",
+              width: "100%",
+              border: "none",
+            }}
+          ></iframe>
+        </div>
         {/* <div className="p-3 mx-auto max-w-2xl text-lg">
           <p className="my-5">This site is currently under construction. Contact form coming soon!</p>
           <p>
